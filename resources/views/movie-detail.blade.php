dc3c16b9cfa2fccb727583ef25643fadf247be66
Alain Van Driessche <alainvd@gmail.com>
2020-09-29 12:59:56 +0200

    Movie Details

--- resources/views/movie-detail.blade.php
+++ resources/views/movie-detail.blade.php
@@ -121,7 +121,7 @@
                     <div class="grid grid-cols-9 gap-6 mt-16">
 
                         <div class="col-start-1 col-span-4">
-                            <label for="imdb" class="block text-sm font-medium leading-5 text-gray-800">IMDB URL - <a target="_blank" href="{{$movie->imdb_url}}">Visit</a></label>
+                            <label for="imdb" class="block text-sm font-medium leading-5 text-gray-800">IMDB URL - <a target="_blank" href="{{$movie->imdb_url}}" class="text-indigo-600 hover:text-indigo-900">Visit</a></label>
                             <input id="original_title"
                                    class="mt-1 form-input block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                                    value="{{$movie->imdb_url}}"
