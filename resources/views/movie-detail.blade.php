157b079eeb075aa1a7171825d0eeb15a9ab604ec
Alain Van Driessche <alainvd@gmail.com>
2020-09-29 12:57:49 +0200

    Movie Details

--- resources/views/movie-detail.blade.php
+++ resources/views/movie-detail.blade.php
@@ -30,7 +30,7 @@
                         </div>
 
                         <div class="col-start-7 col-end-9">
-                            <label for="status" class="block text-sm font-medium leading-5 text-gray-700">Europen Flag
+                            <label for="status" class="block text-sm font-medium leading-5 text-gray-700">European Flag
                                 Status</label>
                             <select id="status"
                                     class="mt-1 block form-select w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5">
