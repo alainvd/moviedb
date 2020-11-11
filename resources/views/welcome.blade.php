c23c565c918af687340587ea9b188988f68f72be
Mikelis Zalais <mikelis@prog.lv>
2020-11-11 21:15:46 +0200

    Init Alpine and layout tweeks

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,6 @@
     <a href="{{route('dossiers')}}">Public Gomepage (Your dossiers)</a><br />
     <a href="{{route('internal')}}">Internal Homepage</a><br />
     <a href="{{route('movies')}}">Movies</a><br />
-    <a href="{{route('movie_detail', ['movie'=>17271])}}">Movie detail</a><br />
+    <a href="{{route('movie_detail', ['movie'=>1])}}">Movie detail</a><br />
 
 </x-layout>
