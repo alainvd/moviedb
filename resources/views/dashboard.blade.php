21f9b795f1d9dc719299865c62465619afe81d29
Alain Van Driessche <alainvd@gmail.com>
2020-11-12 15:55:32 +0100

    Dossiers

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
             <div class="md:flex md:items-center md:justify-between">
                 <div class="flex-1 min-w-0">
                     <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
-                        Dossiers
+                        Welcome {{auth()->user()->name}}
                     </h2>
                 </div>
             </div>
