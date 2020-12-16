2d399ef2c39f2daa95b0e9471992b4d48908af70
Bogdan Calin <calin.s.bogdan@gmail.com>
2020-12-16 12:14:34 +0200

    Factory changes; comment videogame seeder temporarily

--- database/factories/TitleFactory.php
+++ database/factories/TitleFactory.php
@@ -23,7 +23,7 @@ class TitleFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name,
+            'name' => $this->faker->jobTitle,
         ];
     }
 }
