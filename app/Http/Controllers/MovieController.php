18a8b115e4c4e5af91fa360305069e82f9a80c8e
Alain Van Driessche <alainvd@gmail.com>
2020-12-16 14:14:11 +0100

    Simple Pagination

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,7 +14,7 @@ class MovieController extends Controller
      */
     public function index()
     {
-        $movies = Movie::paginate(30);
+        $movies = Movie::simplePaginate(30);
 
         return view('movies', compact(['movies']));
     }
