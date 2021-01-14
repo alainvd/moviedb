9721079ae451f1a1d51abc0089056af6096f64c3
Mikelis Zalais <mikelis@prog.lv>
2021-01-14 11:10:18 +0200

    Remove sidebar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,24 +27,22 @@
     @livewireStyles
 </head>
 
-<body class="bg-gray-100 h-full antialiased leading-none">
+<body class="h-full antialiased leading-none bg-gray-100">
     <div>
 
         @livewire('navbar')
 
         <div class="flex overflow-hidden bg-gray-100">
-            @livewire('sidebar')
-
-            <div class="flex flex-col w-0 flex-1 overflow-hidden">
-                <div class="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+            <div class="flex flex-col flex-1 w-0 overflow-hidden">
+                <div class="pt-1 pl-1 md:hidden sm:pl-3 sm:pt-3">
                     <button class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150" aria-label="Open sidebar">
-                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     </button>
                 </div>
                 <!-- The page content -->
-                <main class="flex-1 relative z-0 overflow-y-auto focus:outline-none" tabindex="0">
+                <main class="relative z-0 flex-1 overflow-y-auto focus:outline-none" tabindex="0">
                     {{ $slot }}
                 </main>
             </div>
