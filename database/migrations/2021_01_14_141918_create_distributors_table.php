2f49090bdce0cd84df0c6000c667f08170265d01
Bogdan Calin <calin.s.bogdan@gmail.com>
2021-01-15 13:52:26 +0200

    Distributor model and migration

---
+++ database/migrations/2021_01_14_141918_create_distributors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistributorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('distributors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('country_id');
+            $table->string('name');
+            $table->string('role')->nullable();
+            $table->date('forecast_release_date');
+            $table->integer('forecast_grant')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updatedBy')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('distributors');
+    }
+}
