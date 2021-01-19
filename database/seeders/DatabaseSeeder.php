366d716dbf47a29faac4947d77edfc52cdda4e23
Mikelis Zalais <mikelis@prog.lv>
2021-01-19 12:01:55 +0200

    Person, Producer, Title updates.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -33,7 +33,7 @@ class DatabaseSeeder extends Seeder
         $this->call(\Database\Seeders\StepDefinitionSeeder::class);
         $this->call(\Database\Seeders\ChecklistSeeder::class);
 
-        //$this->call(\Database\Seeders\TitleSeeder::class);
+        $this->call(\Database\Seeders\TitleSeeder::class);
         $this->call(\Database\Seeders\PersonSeeder::class);
         $this->call(\Database\Seeders\CrewSeeder::class);
 
