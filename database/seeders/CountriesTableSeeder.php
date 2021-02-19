0d77be902c31b1d3829bfe22ffbc29c7695543be
Alain Van Driessche <alainvd@gmail.com>
2021-02-19 09:44:07 +0100

    Countries Table

---
+++ database/seeders/CountriesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Eloquent;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CountriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeders.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
+
+        $path = 'database/seeders/countries.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Countries table seeded!');
+    }
+}
