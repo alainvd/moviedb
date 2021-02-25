70f3f232ea79478c48c557a971c28051ac42fd05
Alain Van Driessche <alainvd@gmail.com>
2021-02-25 16:22:35 +0100

    Comment wrong route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
     <a href="{{route('homepage')}}">FromSEP Homepage</a><br />
     <a href="{{route('dossiers-public')}}">Public Homepage (Your dossiers test)</a><br />
-    <a href="#">Movies Table</a><br />
-    <a href="{{route('table_dossiers')}}">Dossiers Table</a><br />
+{{--    <a href="#">Movies Table</a><br />--}}
+{{--    <a href="{{route('table_dossiers')}}">Dossiers Table</a><br />--}}
     <br />
     <a href="{{route('table_edit_examples')}}">Table edit samples</a><br />
     <br />
