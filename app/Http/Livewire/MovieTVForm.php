02bff9ae1ac7100376728e2198632849db200ec4
Mikelis Zalais <mikelis@prog.lv>
2021-03-17 10:55:07 +0200

    Comment

--- app/Http/Livewire/MovieTVForm.php
+++ app/Http/Livewire/MovieTVForm.php
@@ -150,7 +150,7 @@ class MovieTVForm extends FicheMovieFormBase
         foreach ($messages as $message) $specialErrors->add('crewErrorMessages', $message);
         // Validate subform: if all item fields are filled
         $messages = FormHelpers::validateTableEditItems($this->isEditor, $this->crews, TableEditMovieCrews::class, function($crew) {return Title::find($crew['title_id'])->name;});
-        // Additional message
+        // Additional message before missing field messages
         if ($messages) {$specialErrors->add('crewErrorMessages', 'Please input TBC for mandatory roles not defined.');}
         foreach ($messages as $message) $specialErrors->add('crewErrorMessages', $message);
 
