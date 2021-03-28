027c4a28ff45a5b631af80c809abc1ad79be3c7d
Mikelis Zalais <mikelis@prog.lv>
2021-03-28 23:58:26 +0300

    Reset dependent fields

--- app/Http/Livewire/FicheMovieFormBase.php
+++ app/Http/Livewire/FicheMovieFormBase.php
@@ -101,15 +101,15 @@ class FicheMovieFormBase extends FicheFormBase
         // clear dependent fields
         if ($name == 'movie.link_applicant_work') {
             if ($value !== 'WRKPERS') {
-                $this->movie->link_applicant_work_person_name = '';
-                $this->movie->link_applicant_work_person_position = '';
-                $this->movie->link_applicant_work_person_credit = '';
+                $this->movie->link_applicant_work_person_name = NULL;
+                $this->movie->link_applicant_work_person_position = NULL;
+                $this->movie->link_applicant_work_person_credit = NULL;
             }
         }
         if ($name == 'movie.rights_origin_of_work') {
             if ($value !== 'ADAPTATION') {
-                $this->movie->rights_adapt_author_name = '';
-                $this->movie->rights_adapt_original_title = '';
+                $this->movie->rights_adapt_author_name = NULL;
+                $this->movie->rights_adapt_original_title = NULL;
                 $this->movie->rights_adapt_contract_type = NULL;
                 $this->movie->rights_adapt_contract_start_date = NULL;
                 $this->movie->rights_adapt_contract_end_date = NULL;
@@ -118,8 +118,8 @@ class FicheMovieFormBase extends FicheFormBase
         }
         if ($name == 'movie.film_type') {
             if ($value !== 'SERIES') {
-                $this->movie->number_of_episodes = '';
-                $this->movie->length_of_episodes = '';
+                $this->movie->number_of_episodes = NULL;
+                $this->movie->length_of_episodes = NULL;
             }
         }
     }
