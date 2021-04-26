caffacb6f2717a1853c4913ac9d65233192f4423
Bogdan Calin <calin.s.bogdan@gmail.com>
2021-04-26 19:41:46 +0300

    Updated status list

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -14,6 +14,28 @@ class Status extends Model
 
     public $fillable = [
         'name',
-        'public'
+        'dev',
+        'distri',
+        'dossier',
     ];
+
+    public function scopeForAction($query, $action)
+    {
+        switch ($action) {
+            case 'DEVSLATE':
+            case 'DEVSLATEMINI':
+            case 'DEVVG':
+            case 'CODEVELOPMENT':
+            case 'TV':
+                return $query->where('dev', true);
+            case 'DISTSEL':
+            case 'DISTSAG':
+                return $query->where('dist', true);
+        }
+    }
+
+    public function scopeForDossier($query)
+    {
+        return $query->where('dossier', true);
+    }
 }
