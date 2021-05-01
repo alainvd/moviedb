bb5933de25a46d996c047f4a0c92121f2a8c805f
Mikelis Zalais <mikelis@prog.lv>
2021-05-01 22:22:51 +0300

    Applicant logout link

--- resources/views/partials/ecl/site-header.blade.php
+++ resources/views/partials/ecl/site-header.blade.php
@@ -5,12 +5,12 @@
         <div class="ecl-site-header-harmonised__action">
           <div class="ecl-site-header-harmonised__login-container"><a class="ecl-link ecl-link--standalone ecl-site-header-harmonised__login-toggle" href="/example" data-ecl-login-toggle="true" aria-controls="login-box-id" aria-expanded="false"><svg focusable="false" aria-hidden="true" class="ecl-site-header-harmonised__icon ecl-icon ecl-icon--s">
                 <use xlink:href="/ecl-images/icons/sprites/icons.svg#general--logged-in"></use>
-              </svg>Log in<svg focusable="false" aria-hidden="true" class="ecl-site-header-harmonised__login-arrow ecl-icon ecl-icon--xs">
+              </svg>{{ Auth::user() ? Auth::user()->name : 'Log in' }}<svg focusable="false" aria-hidden="true" class="ecl-site-header-harmonised__login-arrow ecl-icon ecl-icon--xs">
                 <use xlink:href="/ecl-images/icons/sprites/icons.svg#ui--corner-arrow"></use>
               </svg></a>
             <div id="login-box-id" class="ecl-site-header-harmonised__login-box" data-ecl-login-box="true">
               <p class="ecl-site-header-harmonised__login-description">Logged in as {{ Auth::user() ? Auth::user()->name : '' }}</p>
-              <hr class="ecl-site-header-harmonised__login-separator" /><a href="/example" class="ecl-link ecl-link--standalone">Log out</a>
+              <hr class="ecl-site-header-harmonised__login-separator" /><a href="{{ route('cas-logout') }}" class="ecl-link ecl-link--standalone">Log out</a>
             </div>
           </div><!--<a class="ecl-link ecl-link--standalone ecl-site-header-harmonised__language-selector" href="/example" aria-label="English" data-ecl-language-selector="true" aria-controls="language-list-overlay"><span class="ecl-site-header-harmonised__language-icon"><svg focusable="false" aria-hidden="true" class="ecl-site-header-harmonised__icon ecl-icon ecl-icon--s">
                 <use xlink:href="/ecl-images/icons/sprites/icons.svg#general--language"></use>
