fd1e9faeb78ad19631a09895da67afd0e6ddb8d2
Mikelis Zalais <mikelis@prog.lv>
2021-05-06 16:39:39 +0300

    Update instruction file names

--- resources/views/dossiers/instructions/index.blade.php
+++ resources/views/dossiers/instructions/index.blade.php
@@ -1,8 +1,8 @@
 @if (Auth::user()->hasRole('applicant'))
     <div class="w-full mt-8">
-        <h3 class="text-lg text-gray-900 text-left leading-10">Instructions</h3>
+        <h3 class="text-lg leading-10 text-left text-gray-900">Instructions</h3>
 
-        <p class="text-md text-gray-900 text-left leading-6">
+        <p class="leading-6 text-left text-gray-900 text-md">
             In order to complete your application for funding under the Creative Europe MEDIA programme, the information on the audiovisual works being part of the application needs to be created and added.
         </p>
 
@@ -12,11 +12,11 @@
         @elseif ($dossier->action->name === 'DEVSLATE')
             @include('dossiers.instructions.devslate')
         @elseif ($dossier->action->name === 'DEVMINISLATE')
-            @include('dossiers.instructions.DEVMINISLATE')
+            @include('dossiers.instructions.devminislate')
         @elseif ($dossier->action->name === 'CODEV')
-            @include('dossiers.instructions.devco')
+            @include('dossiers.instructions.codev')
         @elseif ($dossier->action->name === 'TVONLINE')
-            @include('dossiers.instructions.tv')
+            @include('dossiers.instructions.tvonline')
         @endif
     @endif
 </div>
