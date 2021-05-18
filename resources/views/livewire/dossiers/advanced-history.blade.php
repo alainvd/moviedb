0068c368d0e941d2aea4221c1faf0eef0541c4f7
Bogdan Calin <calin.s.bogdan@gmail.com>
2021-05-18 15:44:53 +0300

    History close and back buttons

--- resources/views/livewire/dossiers/advanced-history.blade.php
+++ resources/views/livewire/dossiers/advanced-history.blade.php
@@ -1,4 +1,14 @@
-<div class="mt-8">
+<div class="mt-8 mb-16">
+
+    @if (Auth::user()->can('view advanced history'))
+        <a href="{{ $backUrl }}" class="mb-4 outline-none text-blue-500 text-md font-normal tracking-wide flex align-middle">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
+            </svg>
+            Go back
+        </a>
+    @endif
+
     <x-table>
         <x-slot name="head">
             <x-table.heading>Description</x-table.heading>
