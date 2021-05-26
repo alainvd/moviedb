05fe408008db9001904ab57f9d05f8bdde8226ec
Mikelis Zalais <mikelis@prog.lv>
2021-05-27 02:02:11 +0300

    Split import in two parts: Prod (for Dist fiches) and Other (for importing historical fiches and dossiers)

--- app/Console/Commands/Importer/Prod.php
+++ app/Console/Commands/Importer/Prod.php
@@ -4,21 +4,21 @@ namespace App\Console\Commands\Importer;
 
 use Illuminate\Console\Command;
 
-class All extends Command
+class Prod extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'import:all';
+    protected $signature = 'import:prod';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Import all';
+    protected $description = 'Import prod';
 
     /**
      * Create a new command instance.
