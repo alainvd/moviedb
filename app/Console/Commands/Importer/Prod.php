a8728983f00ddebdecd2f70d86875090332b1260
Mikelis Zalais <mikelis@prog.lv>
2021-06-02 11:09:36 +0300

    Still keep user seed

--- app/Console/Commands/Importer/Prod.php
+++ app/Console/Commands/Importer/Prod.php
@@ -61,10 +61,10 @@ class Prod extends Command
         $this->call(\Database\Seeders\RolesAndPermissionsSeeder::class);
         $this->info("roles and permissions seed ok");
 
-        if(!App::environment('production') && !App::environment('acceptance')) {
+        // if(!App::environment('production') && !App::environment('acceptance')) {
             $this->call(\Database\Seeders\UserSeeder::class);
             $this->info("user seed ok");
-        }
+        // }
 
         $this->call('import:movies-dist'); // MoviesDist.php
         $this->info("movies-dist import ok");
