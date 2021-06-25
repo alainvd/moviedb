c8e49ce7cd4c70893c4e00697c7eda42225349be
Mikelis Zalais <mikelis@prog.lv>
2021-06-25 09:53:10 +0300

    Fix home link

--- resources/views/partials/ecl/page-header.blade.php
+++ resources/views/partials/ecl/page-header.blade.php
@@ -3,7 +3,7 @@
       <div class="ecl-container">
         <nav data-ecl-auto-init="BreadcrumbHarmonised" class="ecl-page-header-harmonised__breadcrumb ecl-breadcrumb-harmonised" aria-label="You are here:" data-ecl-breadcrumb-harmonised="true">
           <ol class="ecl-breadcrumb-harmonised__container">
-            <li class="ecl-breadcrumb-harmonised__segment" data-ecl-breadcrumb-harmonised-item="static" aria-hidden="false"><a href="/homepage" class="ecl-breadcrumb-harmonised__link ecl-link ecl-link--standalone">Home</a><svg focusable="false" aria-hidden="true" role="presentation" class="ecl-breadcrumb-harmonised__icon ecl-icon ecl-icon--2xs ecl-icon--rotate-90">
+            <li class="ecl-breadcrumb-harmonised__segment" data-ecl-breadcrumb-harmonised-item="static" aria-hidden="false"><a href="{{ route('root') }}" class="ecl-breadcrumb-harmonised__link ecl-link ecl-link--standalone">Home</a><svg focusable="false" aria-hidden="true" role="presentation" class="ecl-breadcrumb-harmonised__icon ecl-icon ecl-icon--2xs ecl-icon--rotate-90">
                 <use xlink:href="/ecl-images/icons/sprites/icons.svg#ui--corner-arrow"></use>
               </svg></li>
             @foreach ($crumbs as $crumb)
