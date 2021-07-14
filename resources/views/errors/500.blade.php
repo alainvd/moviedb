0574143bbacde6613ba84b8b4d568346abfe02f1
Bogdan Calin <calin.s.bogdan@gmail.com>
2021-07-14 18:53:34 +0300

    Error page message change

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,1 +1,24 @@
-@extends('errors.403')
+@extends('errors.base')
+
+@section('content')
+    <div class="md:w-1/2 px-8 text-center md:text-left">
+        <h2 class="text-4xl font-bold tracking-wide">
+            Something went wrong!
+        </h2>
+        <h3 class="my-4 text-3xl font-normal tracking-normal">
+            Please try again later!
+            <br><br>
+            If the problem persists, please write to us at:
+            <br>
+            <a class="text-indigo-700 hover:text-indigo-500" href="mailto: EACEA-MEDIA-DB@ec.europa.eu">EACEA-MEDIA-DB@ec.europa.eu</a>
+        </h3>
+        <x-anchors.primary
+            class="mt-4"
+            :url="url('/')">
+            Go to homepage
+        </x-anchors.primary>
+    </div>
+    <div class="md:w-1/2 flex items-center">
+        <img src="/images/undraw_videographer.png" alt="Videographer">
+    </div>
+@endsection
